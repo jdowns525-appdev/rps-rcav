4dbacb4c84bfeed6e9db199ebd06fd170781eca2
jdowns525 <downsjosseph@gmail.com>
2023-02-21 20:50:44 +0000

    added yield

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,56 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout()
 
   # Add your actions below this line
   # ================================
   def homepage
     render({ :template => "game_templates/rules.html.erb"})
   end
+#this is rock
   def play_rock
-    render({ :template => "game_templates/user_rock.html.erb".html_safe })
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+     if @comp_move == "rock" 
+      @outcome = "tied"
+    elsif @comp_move == "paper" 
+      @outcome = "lost"
+    elsif @comp_move == "scissors" 
+      @outcome = "won"
+    end 
+
+    render({ :template => "game_templates/user_rock.html.erb", :layout => "wrapper.html.erb" })
   end
+#this is paper
   def play_paper
-    <% comp_move = ["rock", "paper", "scissors"].sample %>
+    @comp_move = ["rock", "paper", "scissors"].sample
 
-    <% if comp_move == "paper" %>
-      <h2>We tied!</h2>
-    <% elsif comp_move == "scissors" %>
-      <h2>We lost!</h2>
-    <% elsif comp_move == "rock" %>
-      <h2>We won!</h2>
-    <% end %>
+     if @comp_move == "rock" 
+      @outcome = "won"
+    elsif @comp_move == "paper" 
+      @outcome = "tied"
+    elsif @comp_move == "scissors" 
+      @outcome = "lost"
+    end 
 
-    render({ :template => "game_templates/user_paper.html.erb".html_safe })
+    render({ :template => "game_templates/user_paper.html.erb" })
   end
+#this is scissors
   def play_scissors
-    render({ :template => "game_templates/user_scissors.html.erb".html_safe})
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+     if @comp_move == "rock" 
+      @outcome = "lost"
+    elsif @comp_move == "paper" 
+      @outcome = "won"
+    elsif @comp_move == "scissors" 
+      @outcome = "tied"
+    end 
+
+    render({ :template => "game_templates/user_scissors.html.erb" })
   end
+
+  def rules
+    render({ :template => "game_templates/rules.html.erb" })
+  end
+
 end
