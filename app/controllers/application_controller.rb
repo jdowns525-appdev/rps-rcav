0803673dbaa4e70309483be3ac8726b9da756a02
jdowns525 <downsjosseph@gmail.com>
2023-02-21 18:17:42 +0000

    user_scissors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,19 @@ class ApplicationController < ActionController::Base
     render({ :template => "game_templates/user_rock.html.erb".html_safe })
   end
   def play_paper
+    <% comp_move = ["rock", "paper", "scissors"].sample %>
+
+    <% if comp_move == "paper" %>
+      <h2>We tied!</h2>
+    <% elsif comp_move == "scissors" %>
+      <h2>We lost!</h2>
+    <% elsif comp_move == "rock" %>
+      <h2>We won!</h2>
+    <% end %>
+
     render({ :template => "game_templates/user_paper.html.erb".html_safe })
+  end
+  def play_scissors
+    render({ :template => "game_templates/user_scissors.html.erb".html_safe})
   end
 end
